c975d12753566ffd31ecb213b1002729d7287460
Huaxichi <huaxichi@qq.com>
2025-01-26 22:39:36 +0800

    Site updated: 2025-01-26 22:39:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["7b5c3ca4/"];function toRandomPost(){
+var posts=["7b5c3ca4/","1ac9df6e/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
