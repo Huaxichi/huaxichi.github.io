40e4c9705a837e9d268313a075b2edd5a050224e
Huaxichi <huaxichi@qq.com>
2025-02-22 10:46:05 +0800

    Site updated: 2025-02-22 10:46:05

--- js/utils.js
+++ js/utils.js
@@ -845,7 +845,7 @@ const anzhiyu = {
       return;
     }
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "12560878374";
+    const userId = "13350491765";
     const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
